795b92ecbcba0224a0cee8d94265ac41688df312
DamjanHatrix <damjan.trickovic@students.finki.ukim.mk>
2018-03-07 15:46:17 +0100

    added model.archive classes

--- src/main/java/com/finki/soa/group3/HRSystem/model/archive/Director.java
+++ src/main/java/com/finki/soa/group3/HRSystem/model/archive/Director.java
@@ -1,4 +1,19 @@
 package com.finki.soa.group3.HRSystem.model.archive;
 
-public class Director {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "Director")
+public class Director extends HrWorker{
+
+    public Director() {
+    }
+
+    public Director(String firstName, String lastName, String EMBG, List<Document> contractsSigned) {
+        super(firstName, lastName, EMBG, contractsSigned);
+    }
+
+
 }
--- src/main/java/com/finki/soa/group3/HRSystem/model/archive/HrWorker.java
+++ src/main/java/com/finki/soa/group3/HRSystem/model/archive/HrWorker.java
@@ -1,4 +1,25 @@
 package com.finki.soa.group3.HRSystem.model.archive;
 
-public class HRUser {
+import com.finki.soa.group3.HRSystem.model.Employee;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "HrPersonal")
+public class HrWorker extends Employee {
+
+    @OneToMany
+    List<Document> contractsSigned;
+
+    public HrWorker() {
+
+    }
+
+    public HrWorker(String firstName, String lastName, String EMBG, List<Document> contractsSigned) {
+        super(firstName, lastName, EMBG);
+        this.contractsSigned = contractsSigned;
+    }
 }
