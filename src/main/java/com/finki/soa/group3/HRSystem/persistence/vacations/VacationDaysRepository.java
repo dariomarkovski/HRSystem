75bbd8e0cd79fa8943232ba1559684b8adb9b604
Ferid <ferid.dragon@hotmail.com>
2018-03-18 22:14:06 +0100

    Fix invalid queries in vacation related repositories.

--- src/main/java/com/finki/soa/group3/HRSystem/persistence/vacations/VacationDaysRepository.java
+++ src/main/java/com/finki/soa/group3/HRSystem/persistence/vacations/VacationDaysRepository.java
@@ -5,22 +5,24 @@ import com.finki.soa.group3.HRSystem.model.vacations.VacationDays;
 import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VacationDaysRepository extends CrudRepository<VacationDays, Long> {
 
-    @Query("SELECT SUM(amount) FROM VacationDays WHERE employee.id = :id AND "
+    @Query("SELECT SUM(vd.amount) FROM VacationDays vd WHERE employee.id = :id AND "
             + "validFrom < :startDate AND validTo > :endDate")
     int sumVacationDaysForEmployeeExclusive(Long id, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("SELECT SUM(amount) FROM VacationDays WHERE employee.id = :id AND "
+    @Query("SELECT SUM(vd.amount) FROM VacationDays vd WHERE employee.id = :id AND "
             + "((:startDate > validFrom AND :startDate < validTo) OR (:endDate > validFrom AND :endDate < validTo))")
     int sumVacationDaysForEmployee(Long id, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("SELECT * FROM VacationDays WHERE employee.id = :id AND " + "validFrom < :startDate AND validTo > :endDate")
+    @Query("SELECT vd FROM VacationDays vd WHERE employee.id = :id AND " + "validFrom < :startDate AND validTo > :endDate")
     Iterable<VacationDays> findVacationDaysForEmployeeExclusive(Long id, LocalDateTime startDate,
             LocalDateTime endDate);
 
-    @Query("SELECT * FROM VacationDays WHERE employee.id = :id AND "
+    @Query("SELECT vd FROM VacationDays vd WHERE employee.id = :id AND "
             + "((:startDate > validFrom AND :startDate < validTo) OR (:endDate > validFrom AND :endDate < validTo))")
     Iterable<VacationDays> findVacationDaysForEmployee(Long id, LocalDateTime startDate, LocalDateTime endDate);
 
